16be8c6e2eeab6bd9ce31e9ee2879b9920e80c29
Sanjana Mahmood <74899722+smahmood00@users.noreply.github.com>
2023-02-23 00:27:47 +0800

    Update datasets.py

--- datasets.py
+++ datasets.py
@@ -17,7 +17,7 @@ OUTPUT_DIR = 'alexnet_data_out'
 
 def create_dataset():
     transformation = transforms.Compose([
-            transforms.CenterCrop(IMAGE_DIM),
+            transforms.Resize((227,227)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
